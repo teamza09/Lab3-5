9ba83af09abb1de82abbe70678ffe766d7fcbb61
teamza09 <jetsadaporn.1301@gmail.com>
2020-08-14 16:40:40 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{ 
+	  float B,H,Ans;
+  	printf("Enter B : ");
+  	scanf("%f",&B);		 
+  	printf("Enter H : ");
+  	scanf("%f",&H);	
+	  Ans=(B*H)*1/2;	 		 
+  	printf("Area of a Triangle is %0.2f",Ans);
+    return 0;
+}
